46aa8eeda7c68e8a8a6e39f461eb335622b962a7
daivikgarg2 <128111734+daivikG02@users.noreply.github.com>
2023-03-18 15:41:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import turtle
+
+t = turtle.Pen()
+t.speed(0)
+t.width(4)
+t.pencolor("orange")
+for x in range(20):
+    t.forward(x*4)
+    t.left(90)
+t.pencolor("dark green")
+for x in range(20):
+    t.forward(x*3)
+    t.left(90)
+t.pencolor("light blue")
+for x in range(20):
+    t.forward(x*4)
+    t.left(90)
+t.pencolor("pink")
+for x in range(20):
+    t.forward(x*3)
+    t.left(90)
+t.pencolor("red")
+for x in range(20):
+    t.forward(x*4)
+    t.left(90)
+t.pencolor("yellow")
+for x in range(20):
+    t.forward(x*3)
+    t.left(90)
+t.pencolor("brown")
+for x in range(20):
+    t.forward(x*4)
+    t.left(90)
+t.pencolor("grey")
+for x in range(20):
+    t.forward(x*3)
+    t.left(90)
+t.pencolor("cyan")
+for x in range(20):
+    t.forward(x*4)
+    t.left(90)
+t.pencolor("brown")
+for x in range(20):
+    t.forward(x*3)
+    t.left(90)
+t.pencolor("grey")
+for x in range(20):
+    t.forward(x*4)
+    t.left(90)
+t.pencolor("violet")
+for x in range(20):
+    t.forward(x*3)
+    t.right(90)
+t.pencolor("indigo")
+for x in range(20):
+    t.forward(x*4)
+    t.right(90)
+t.pencolor("cyan")
+for x in range(20):
+    t.forward(x*3)
+    t.right(90)
+t.pencolor("orange")
+for x in range(20):
+    t.forward(x*4)
+    t.right(90)
+t.pencolor("lime")
+for x in range(20):
+    t.forward(x*3)
+    t.right(90)
+t.pencolor("maroon")
+for x in range(20):
+    t.forward(x*4)
+    t.right(90)
+t.pencolor("navy")
+for x in range(20):
+    t.forward(x*3)
+    t.right(90)
+t.pencolor("purple")
+for x in range(20):
+    t.forward(x*3)
+    t.right(90)
